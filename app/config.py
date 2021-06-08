d289a9da34a3ae7667d8d20c54e51a54dc67e488
Rich Cooper <richcoop@cisco.com>
2021-06-08 15:33:50 +0100

    Remove useless DEBUG setting in config, and rename configs

--- app/config.py
+++ app/config.py
@@ -9,16 +9,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     CONFIG_NAME = "base"
     USE_MOCK_EQUIVALENCY = False
-    DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
-    CONFIG_NAME = "dev"
+    CONFIG_NAME = "development"
     SECRET_KEY = os.getenv(
         "DEV_SECRET_KEY", "Super secret dev key..."
     )
-    DEBUG = True
     TESTING = False
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/app-dev.db"
 
@@ -26,15 +24,13 @@ class DevelopmentConfig(BaseConfig):
 class TestingConfig(BaseConfig):
     CONFIG_NAME = "test"
     SECRET_KEY = os.getenv("TEST_SECRET_KEY", "Super secret test key...")
-    DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/app-test.db"
 
 
 class ProductionConfig(BaseConfig):
-    CONFIG_NAME = "prod"
+    CONFIG_NAME = "production"
     SECRET_KEY = os.getenv("PROD_SECRET_KEY", "Super secret prod key...")
-    DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/app-prod.db"
 
